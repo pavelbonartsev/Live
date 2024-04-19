95f36ca45eb475bb45c7f52b2cffabcb32fde5e2
pavelbonartsev <150126230+pavelbonartsev@users.noreply.github.com>
2024-04-19 12:50:58 +0300

    Update kvest.py

--- kvest.py
+++ kvest.py
@@ -10,7 +10,7 @@ while flg == 0:
 a, copy_a = [[0 for i in range(b)] for j in range(c)], []
 def pr():
     for i in range(len(a)):
-        print(*copy_a[i], sep="")
+        print(#*copy_a[i], sep="")
     print()
 def isInField(x, y):
     if x <= len(a[0]) - 1 and x >= 0 and y >= 0 and y <= len(a) - 1:
@@ -44,18 +44,22 @@ for i in range(e):
         print("На поле нет ни одной живой клетки, программа прерывается.")
         break
     flg1 = 0
-    copy_a = list(map(list, a))
+    copy_a = [[0 for i in range(len(a[0]))] for j in range(len(a))]
     for j in range(c):
         for k in range(b):
             if a[j][k] == 0 and returnNeighbours(k, j) == 3:
                 copy_a[j][k] = 1
-            elif a[j][k] == 1 and returnNeighbours(k, j) < 2 and returnNeighbours(k, j) > 3:
+            elif a[j][k] != 0 and returnNeighbours(k, j) != 3:
                 copy_a[j][k] = 0
-            pr()
-            tm.sleep(0.4)
-    a = list(map(list, copy_a))
+            if a[j][k] == 1 and returnNeighbours(k, j) < 2 and returnNeighbours(k, j) > 3:
+                copy_a[j][k] = 0
+            elif a[j][k] == 1 and returnNeighbours(k, j) in [2, 3]:
+                copy_a[j][k] = 1
+    pr()
+    tm.sleep(0.4)
+    a = copy_a.copy()
     #if a in evolutions:
-        #print("Складывается периодическая конфигурация, программа прерывается.")
+        #print("Складывается периодическая конфигурация,# программа прерывается.")
         #break
     for i in range(len(a)):
         for j in range(len(a[0])):
